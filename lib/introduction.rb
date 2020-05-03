f7549655b1971e35cec3ab8d58853f82074fc3ff
Justin James <justin.james@yale.edu>
2020-05-03 13:32:49 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -14,7 +14,7 @@ def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{lanugage}."
 end
 
-introductiion_with_language "Marie" "React"
+introduction_with_language "Marie" "React"
 
 def introduction_with_language_optional (name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{lanugage}."
